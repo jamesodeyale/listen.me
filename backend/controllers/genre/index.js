e334f423dd6fa82d332158af22a8ae93afbf6c8b
devjamesd <jamesodeyale01@gmail.com>
2021-05-11 10:57:09 -0400

    completed edit genre endpoint

--- backend/controllers/genre/index.js
+++ backend/controllers/genre/index.js
@@ -25,8 +25,35 @@ createGenre = async (req, res) => {
   }
 };
 
+editGenre = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const { genre_id } = req.params;
+    const { rows } = await db.query(
+      `update genre set name=$1 where genre_id=${genre_id} returning *`,
+      [name]
+    );
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        genre: rows[0]
+      }
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "failed",
+      error: {
+        errors: null,
+        message: "Error occurred. Please try again."
+      }
+    });
+  }
+};
+
 const genre = {
-  createGenre
+  createGenre,
+  editGenre
 };
 
 module.exports = {
--- backend/routes/genre/index.js
+++ backend/routes/genre/index.js
@@ -3,4 +3,6 @@ const { genre } = require("../../controllers/genre");
 
 router.route("/create-genre").post(genre.createGenre);
 
+router.route("/edit-genre/:genre_id").put(genre.editGenre);
+
 module.exports = router;
